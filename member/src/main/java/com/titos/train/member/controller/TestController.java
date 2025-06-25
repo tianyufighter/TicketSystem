5e2dc7ca8bd8fa282c5929fd8262816e1a01c13e
tianyufighter <634522023@qq.com>
2025-06-25 16:45:37 +0800

    4.4 修改启动日志,增加/member,修改logback日志

---
+++ member/src/main/java/com/titos/train/member/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.titos.train.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
